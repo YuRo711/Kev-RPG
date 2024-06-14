018fcc4c50ed070856d7bc2d8e0d0058cde21ee3
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-06-14 22:01:04 +0300

    idle state

---
+++ Assets/Scripts/Player/MoveDirections.cs
@@ -0,0 +1,10 @@
+namespace Player
+{
+    public enum MoveDirections
+    {
+        Up = 0,
+        Right = 1,
+        Down = 2,
+        Left = 3,
+    }
+}
--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -4,43 +4,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerControls : MonoBehaviour
+namespace Player
 {
-    #region Fields
-
-    [SerializeField] private float mouseSensitivity = 100f;
-    [SerializeField] private Transform cameraTransform;
-
-    private float _rotationX;
-
-    #endregion
-
-    #region Private Methods
-
-    private void ProcessMouseMovement()
+    public class PlayerControls : MonoBehaviour
     {
-        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        #region Fields
 
-        _rotationX = Math.Clamp(_rotationX - mouseY, -45f, 45f);
+        [SerializeField] private float speed;
+        [SerializeField] private Rigidbody2D playerRigidbody;
+        [SerializeField] private Animator animator;
+
+        private int _animatorDirectionHash;
+        private int _animatorIsMovingHash;
+
+        #endregion
+
+        #region Private Methods
+
+        private void GetKeyInput()
+        {
+            var inputX = Input.GetAxis("Horizontal");
+            var inputY = Input.GetAxis("Vertical");
         
-        transform.Rotate(Vector3.up * mouseX);
-        cameraTransform.localRotation = Quaternion.Euler(_rotationX, mouseX, 0);
+            Move(inputX, inputY);
+        }
+
+        private void Move(float inputX, float inputY)
+        {
+            playerRigidbody.MovePosition(transform.position + new Vector3(inputX, inputY) * 
+                (Time.fixedDeltaTime * speed));
+
+            if (inputX != 0)
+            {
+                animator.SetInteger(_animatorDirectionHash,
+                    inputX > 0 ? (int)MoveDirections.Right : (int)MoveDirections.Left);
+                animator.SetBool(_animatorIsMovingHash, true);
+            }
+            else if (inputY != 0)
+            {
+                animator.SetInteger(_animatorDirectionHash,
+                    inputY > 0 ? (int)MoveDirections.Up : (int)MoveDirections.Down);
+                animator.SetBool(_animatorIsMovingHash, true);
+            }
+            else
+            {
+                animator.SetBool(_animatorIsMovingHash, false);
+            }
+        }
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        private void Awake()
+        {
+            _animatorDirectionHash = Animator.StringToHash("Direction");
+            _animatorIsMovingHash = Animator.StringToHash("IsMoving");
+        }
+
+        private void FixedUpdate()
+        {
+            GetKeyInput();
+        }
+
+        #endregion
     }
-
-    #endregion
-
-    #region MonoBehaviour Callbacks
-
-    private void Awake()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private void Update()
-    {
-        ProcessMouseMovement();
-    }
-
-    #endregion
 }
