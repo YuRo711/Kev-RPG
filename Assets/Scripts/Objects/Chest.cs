bf0bfde4838effd740d381c6424ccda7aa81d839
YuRo711 <rohlinay@mail.ru>
2024-07-21 14:11:23 +0300

    opening chests

--- Assets/Scripts/Objects/Chest.cs
+++ Assets/Scripts/Objects/Chest.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Party;
 using UnityEngine;
 using Utils;
+using Random = UnityEngine.Random;
 
 namespace Objects
 {
@@ -12,17 +15,30 @@ namespace Objects
 
         [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private Sprite openSprite;
-        private bool isOpen;
+        
+        [SerializeField] private int number;
+        private static readonly List<bool> ChestsOpen = new ();
 
         public void OnInteract()
         {
-            if (!IsPlayerInBox || isOpen) return;
+            if (!IsPlayerInBox || ChestsOpen[number]) return;
             
             partyData.money += (int)(Random.value * maxValue);
             moneyEvent.Raise();
 
-            isOpen = true;
+            ChestsOpen[number] = true;
+            OpenSprite();
+        }
+
+        private void OpenSprite()
+        {
             spriteRenderer.sprite = openSprite;
+        }
+
+        private void Start()
+        {
+            if (ChestsOpen.Count <= number)
+                ChestsOpen.Add(false);
         }
     }
 }
